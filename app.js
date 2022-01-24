b70148daa2490b3152b54d97df2d9a1b2dc4c2ef
BenJarrett <b.cl.jarrett@gmail.com>
2022-01-24 14:56:01 -0600

    global objects

---
+++ app.js
@@ -0,0 +1,18 @@
+
+function sayHello(name) {
+
+  console.log('Hello ' + name);
+}
+
+// sayHello('Ben');
+console.log(window);
+
+
+// GLOBAL OBJECTS //
+console.log(); // global //
+
+setTimeout(); // Call function after a delay // 
+clearTimeout(); 
+
+setInterval(); // Repeatedly call a function after a given delay // 
+clearInterval(); // Stop that function from being called repeatdely //
